879257c208cc014e01f515c13754df2f14d5ce60
sahasam <munamalasahas@gmail.com>
2020-08-07 12:15:21 -0700

    add docopt and move code into if __name__...

--- bot.py
+++ bot.py
@@ -1,3 +1,13 @@
+"""
+Livestream clip compiler bot
+
+Usage:
+    bot.py [options] 
+Options:
+    -h, --help                          display this
+    -o, --out-file <file>               specify the output file of the final compilation
+    -t, --time <time>                   specify the length of the compilation 
+"""
 import configparser
 import os
 import praw
@@ -6,29 +16,13 @@ import requests
 import sys
 import urllib.request
 
+from docopt import docopt
+
 config = configparser.ConfigParser()
 config.read("config.ini")
 
 DOWNLAOD_FOLDER = os.path.join(os.path.dirname(__file__), "downloads")
 
-reddit = praw.Reddit(client_id=config['reddit oauth']['client_id'],
-                    client_secret=config['reddit oauth']['client_secret'],
-                    username=config['reddit oauth']['username'],
-                    password=config['reddit oauth']['password'],
-                    user_agent=config['reddit oauth']['user_agent'])
-
-auth_response = requests.post(
-    'https://id.twitch.tv/oauth2/token',
-    data={"client_id": config['twitch oauth']['client_id'],
-            "client_secret": config['twitch oauth']['client_secret'],
-            "grant_type": "client_credentials"}).json()
-
-try:
-    access_token = auth_response['access_token']
-except KeyError as e:
-    print(auth_response)
-    print("failed to get access token from twitch.tv: {e}")
-    sys.exit(1)
 
 
 def retrieve_mp4_data(slug):
@@ -52,21 +46,44 @@ def dl_progress (count, block_size, total_size) :
     sys.stdout.write("\r...%d%%" % percent)
     sys.stdout.flush()
 
-subreddit = reddit.subreddit('LivestreamFails')
-hot_lsf= subreddit.hot(limit=1)
+if __name__ == "__main__" :
+    reddit = praw.Reddit(client_id=config['reddit oauth']['client_id'],
+                        client_secret=config['reddit oauth']['client_secret'],
+                        username=config['reddit oauth']['username'],
+                        password=config['reddit oauth']['password'],
+                        user_agent=config['reddit oauth']['user_agent'])
 
-for submission in hot_lsf :
-    print(submission.title, submission.url.split('/')[3])
+    auth_response = requests.post(
+        'https://id.twitch.tv/oauth2/token',
+        data={"client_id": config['twitch oauth']['client_id'],
+            "client_secret": config['twitch oauth']['client_secret'],
+            "grant_type": "client_credentials"}).json()
 
-    slug = submission.url.split('/')[3]
-    mp4_url, clip_title = retrieve_mp4_data(submission.url.split('/')[3])
-    regex = re.compile('[^a-zA-Z0-9_]')
-    clip_title = clip_title.replace(' ', '_')
-    out_filename = regex.sub('', clip_title) + '.mp4'
-    output_path = os.path.join(DOWNLAOD_FOLDER, out_filename)
+    try:
+        access_token = auth_response['access_token']
+    except KeyError as e:
+        print(auth_response)
+        print("failed to get access token from twitch.tv: {e}")
+        sys.exit(1)
+        args = docopt(__doc__, version="lsfbot v1.0.0")     
+        print(args)
 
-    print('\nDownloading clip slug: ' + slug)
-    print('"' + clip_title + '" -> ' + out_filename)
-    print(mp4_url)
-    urllib.request.urlretrieve(mp4_url, output_path, reporthook=dl_progress)
-    print('\nDone.')
+        subreddit = reddit.subreddit('LivestreamFail')
+        hot_lsf= subreddit.hot(limit=1)
+
+    total_vid_time = 0
+    for submission in hot_lsf :
+        slug = submission.url.split('/')[3]
+        
+
+        mp4_url, clip_title = retrieve_mp4_data(submission.url.split('/')[3])
+        regex = re.compile('[^a-zA-Z0-9_]')
+        clip_title = clip_title.replace(' ', '_')
+        out_filename = regex.sub('', clip_title) + '.mp4'
+        output_path = os.path.join(DOWNLAOD_FOLDER, out_filename)
+
+        print('\nDownloading clip slug: ' + slug)
+        print('"' + clip_title + '" -> ' + out_filename)
+        print(mp4_url)
+        urllib.request.urlretrieve(mp4_url, output_path, reporthook=dl_progress)
+        print('\nDone.')
