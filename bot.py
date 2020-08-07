7d9c7247165fd848cbeb7e8856b84fe86dc7bd4a
sahasam <munamalasahas@gmail.com>
2020-08-06 18:10:24 -0700

    seperate reddit's oauth variables from concat.exe's

--- bot.py
+++ bot.py
@@ -1,14 +1,17 @@
 import praw
+import pycurl
 import configparser
 
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-reddit = praw.Reddit(client_id=config['oauth']['client_id'],
-                    client_secret=config['oauth']['client_secret'],
-                    username=config['oauth']['username'],
-                    password=config['oauth']['password'],
-                    user_agent=config['oauth']['user_agent'])
+reddit = praw.Reddit(client_id=config['reddit oauth']['client_id'],
+                    client_secret=config['reddit oauth']['client_secret'],
+                    username=config['reddit oauth']['username'],
+                    password=config['reddit oauth']['password'],
+                    user_agent=config['reddit oauth']['user_agent'])
+
+curl = pycurl.Curl()
 
 subreddit = reddit.subreddit('LivestreamFails')
 hot_lsf= subreddit.hot(limit=1)
